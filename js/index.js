9a9aa14ef1dfddca3e56abc3201040726e2ab9e9
Cristoballizana099 <Cristoballizana91@gmail.com>
2023-10-25 14:21:49 -0600

    primer commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+
+const carta1 = document.getElementById('carta1');
+const carta2 = document.getElementById('carta2');
+const carta3 = document.getElementById('carta3');
+
+
+carta1.addEventListener('click', () =>{
+    window.location.href = "login.html";
+});
+
+carta2.addEventListener('click', () =>{
+    window.location.href = "login.html";
+});
+
+carta3.addEventListener('click', () =>{
+    window.location.href = "informacion.html";
+});
+
